70fa8609dba23e9749e734fd611c78e4cfcf5d86
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-08-22 12:03:47 +0330

    change rest  function apiview to rest class apiview

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path, include
 from .views import PostList,PostDetail, PostList
+from . import views
 app_name = "api-v1"
 
 urlpatterns = [
-    path("post/", PostList, name="post-list"),
-    path("post/<int:id>/", PostDetail, name="post-detail"),
+    # path("post/", PostList, name="post-list"),
+    # path("post/<int:id>/", PostDetail, name="post-detail"),
+    
+    path('post/', views.PostList.as_view(), name="post-list"),
+    path("post/<int:id>/", views.PostDetail.as_view(), name="post-detail"),
+    
     
 ]
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -6,7 +6,27 @@ from ...models import Post
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView
 
+class PostList(APIView):
+    '''getting a list of posts and creating new post'''
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    def get(self, request):
+        '''retrieves a list of posts'''
+        posts = Post.objects.filter(status=True)
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+    def post(self, request):
+        '''creates a new post with provided data'''
+        serializer = PostSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    
+'''
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
 def PostList(request):
@@ -35,3 +55,30 @@ def PostDetail(request, id):
     elif request.method =="DELETE":
         post.delete()
         return Response({"detail":"Item removed successfully"},status=status.HTTP_204_NO_CONTENT)
+        '''
+
+
+class PostDetail(APIView):
+    '''getting a post, updating it and deleting it'''
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = PostSerializer
+    
+    def get(self, request , id):
+        '''retrieves a single post'''
+        post = get_object_or_404(Post,pk=id, status = True)
+        serializer = self.serializer_class(post)
+        return Response(serializer.data)
+    def put(self, request , id):
+        '''updates a single post'''
+        post = get_object_or_404(Post,pk=id, status = True)
+        serializer = PostSerializer(post, data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, id):
+        '''delete a single post'''
+        post = get_object_or_404(Post,pk=id, status = True)
+        post.delete()
+        return Response({"detail":"Item removed successfully"},status=status.HTTP_204_NO_CONTENT)
+    
