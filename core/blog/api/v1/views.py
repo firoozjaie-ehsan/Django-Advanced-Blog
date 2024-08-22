11baf1d69a675de9dcdacb922d2324f86143edf1
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-08-22 16:41:15 +0330

    improve detail mixin with RetrieveupdatedestroyAPIView

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView, ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework import mixins
 
 # create function based view
@@ -93,17 +93,9 @@ class PostDetail(APIView):
         return Response({"detail":"Item removed successfully"},status=status.HTTP_204_NO_CONTENT)
     '''
     
-class PostDetail(GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+class PostDetail(RetrieveUpdateDestroyAPIView):
     """getting a post, updating it and deleting it"""
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
     
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
