029bf5b9a245d9524109a856a4d1ffc4cd61be6b
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-08-24 15:26:16 +0330

    pagination for model class based view api

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -11,8 +11,8 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from rest_framework import mixins, viewsets
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .pegination import DefaultPagination
 # create function based view
 '''
 @api_view(["GET", "POST"])
@@ -109,9 +109,11 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['category', 'author', 'status']
     search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
+    pagination_class = DefaultPagination
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
