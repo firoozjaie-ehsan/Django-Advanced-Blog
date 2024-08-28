846219e592d1d215b7ebe3ccf462e677693ac87c
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-08-28 18:02:14 +0330

    manage registration for rest

---
+++ core/accounts/api/v1/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from . import views
+urlpatterns = [
+    # registration
+    path("registration/", views.RegistrationApiView.as_view(), name ="registration")
+    
+    #change password
+    
+    #reset password
+    
+    #login token
+    
+    #login jwt
+]
