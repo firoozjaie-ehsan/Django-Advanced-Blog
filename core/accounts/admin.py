77e424b0f25641a9231db2f2bfd79ad813e5fa77
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-08-29 15:21:45 +0330

    verification for user enabled

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -5,14 +5,14 @@ from .models import User, Profile
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     model=User
-    list_display = ("email", "is_superuser", "is_active",)
-    list_filter = ("email", "is_superuser", "is_active",)
+    list_display = ("email", "is_superuser", "is_active","is_verified")
+    list_filter = ("email", "is_superuser", "is_active","is_verified")
 
     searching_fields =("email",)
     ordering = ("email",)
     fieldsets = (
         ("Authentication", {"fields": ("email", "password")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "is_verified")}),
         ("group Permissions", {"fields": ("groups", "user_permissions")}),
         ("important date", {"fields": ("last_login",)}),
     )
@@ -20,7 +20,7 @@ class CustomUserAdmin(UserAdmin):
         (None, {
             "classes": ("wide",),
             "fields": (
-                "email", "password1", "password2", "is_staff","is_active","is_superuser", 
+                "email", "password1", "password2", "is_staff","is_active","is_superuser","is_verified",
             )}
         ),
     )    
