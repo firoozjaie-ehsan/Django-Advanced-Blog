98a4101dea3d225f6d4a93d26f37800c94571b3f
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-08-30 20:16:43 +0330

    SimpleTestCase for django

--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,10 @@
-# from django.test import TestCase
-
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from .views import IndexView, ArticleListView
 # Create your tests here.
+
+class TestUrl(SimpleTestCase):
+    
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:post_list')
+        self.assertEquals(resolve(url).func.view_class, ArticleListView)
