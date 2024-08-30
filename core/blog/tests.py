f031366c68280d10c5f26926bac89f18c641657f
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-08-30 20:17:26 +0330

    Revert "SimpleTestCase for django"
    This reverts commit 98a4101dea3d225f6d4a93d26f37800c94571b3f.

--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,10 +1,3 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from .views import IndexView, ArticleListView
-# Create your tests here.
+# from django.test import TestCase
 
-class TestUrl(SimpleTestCase):
-    
-    def test_blog_index_url_resolve(self):
-        url = reverse('blog:post_list')
-        self.assertEquals(resolve(url).func.view_class, ArticleListView)
+# Create your tests here.
