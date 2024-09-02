7cbdf76b7acd77be4394f6527301027f9cd67b15
ehsan firooz <88386111+firoozjaie-ehsan@users.noreply.github.com>
2024-09-02 10:04:14 +0330

    install pytest and set config

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default = "test")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", cast=bool)
+DEBUG = config("DEBUG", cast=bool, default = True)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
@@ -122,8 +122,6 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 
 USE_I18N = True
-
-USE_L10N = True
 
 USE_TZ = True
 
